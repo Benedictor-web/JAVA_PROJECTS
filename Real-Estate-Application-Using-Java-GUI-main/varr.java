8a8e4269d3ab9e99290b46271a7ad64e88d4086f
BENEDICTOR <143268661+Benedictor-web@users.noreply.github.com>
2025-01-25 13:41:01 +0300

    Delete Real-Estate-Application-Using-Java-GUI-main directory

--- Real-Estate-Application-Using-Java-GUI-main/varr.java
+++
@@ -1,9 +0,0 @@
-package com.example.realestatesystem;
-
-import java.util.ArrayList;
-
-public interface varr {
-    ArrayList<String> Alist = new ArrayList<>();
-    ArrayList<String> Blist = new ArrayList<>();
-    ArrayList<String> Clist = new ArrayList<>();
-}
